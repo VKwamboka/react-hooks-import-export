32b5a08f188d1cc63c925672dfc5cd644a01d0b5
verah <verahnyagaka101@gmail.com>
2022-07-12 17:54:05 +0300

    Assignment completed

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,15 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as AllFeatures from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
-
+  MesaVerde();
+  console.log(AllFeatures.trees)
+  AllFeatures.wildlife();
+  AllFeatures.elevation();
   return <h1>Colorado State Parks!</h1>;
 }
+
+export default ColoradoStateParks
